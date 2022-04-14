acc72f62e44936f86d697f2ed0e0c5e24d4441c6
autf <autf@dev.null>
2022-04-14 13:54:21 +0800

    7.1 merge two sorted LL (in 3 ways)

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -6,8 +6,46 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    ## [[middle speed]]
+    # simulate ptr by array
+    ls = [L1, L2]
+    g = t = ListNode() # g for guardian, t for temp
+    while all(ls):
+        # lambda below is const, not newly build every loop
+        # see bytecode via: https://godbolt.org/
+        i = min(0, 1, key=lambda i: ls[i].data)
+        x = ls[i]
+        ls[i] = x.next
+        t.next = t = x
+    t.next = ls[0] or ls[1]
+    return g.next
+
+    ## [[fastest]]
+    g = t = ListNode()
+    while L1 and L2:
+        if L1.data < L2.data:
+            x = L1
+            L1 = L1.next
+        else:
+            x = L2
+            L2 = L2.next
+        t.next = t = x
+    t.next = L1 or L2
+    return g.next
+
+    ## [[slowest]]
+    import heapq
+    def each(l):
+        while l:
+            yield l.data
+            l = l.next
+    g = t = ListNode()
+    for v in heapq.merge(each(L1), each(L2)):
+        t.next = t = ListNode(v)
+        # x = ListNode(v)
+        # t.next = x
+        # t = x
+    return g.next
 
 
 if __name__ == '__main__':
